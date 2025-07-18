7e235656846f129437cb782cf146d3435480d804
Jailson.Osorio <jailson.osorio@nosi.cv>
2025-07-18 15:26:15 -0100

    metadados into dashboard pages

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,12 +1,8 @@
-//import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-//import { fetchRevenue } from '@/app/lib/data';
 import CardWrapper from '@/app/ui/dashboard/cards';
-import { 
-  fetchCardData 
-} from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
  
